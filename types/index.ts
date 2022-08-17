0a542f60d24656c98ce8f837ffbd4f929a2a335d
İpek Efendiev <ipekefendiev@gmail.com>
2022-08-17 17:32:26 +0300

    add types and update font style

--- types/index.ts
+++ types/index.ts
@@ -5,3 +5,21 @@ export interface IItem {
   url?: string
   tags?: string[]
 }
+
+export type TagType = {
+  id: number
+  attributes: {
+    name: string
+  }
+}
+
+export type WorkHistoryItem = {
+  id: number
+  attributes: {
+    date: string
+    content: string
+    title: string
+    url?: string
+    tags?: { data: TagType[] }
+  }
+}
