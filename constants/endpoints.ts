fa51df4bd6ca855ca65626fc720f2c72a2acaeea
İpek Efendiev <ipekefendiev@gmail.com>
2022-08-17 17:31:02 +0300

    add endpoints to fetch data from cms

---
+++ constants/endpoints.ts
@@ -0,0 +1,13 @@
+const ENDPOINTS = {
+  about: 'https://iefendiev-portfolio-content.herokuapp.com/api/abouts',
+  'work-history':
+    'https://iefendiev-portfolio-content.herokuapp.com/api/contents?populate=*',
+  'tech-stack':
+    'https://iefendiev-portfolio-content.herokuapp.com/api/tech-stacks?populate=*',
+  styling:
+    'https://iefendiev-portfolio-content.herokuapp.com/api/stylings?populate=*',
+  others:
+    'https://iefendiev-portfolio-content.herokuapp.com/api/others?populate=*',
+}
+
+export default ENDPOINTS
