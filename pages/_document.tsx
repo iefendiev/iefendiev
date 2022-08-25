4948585c953f4e7b4c096268a16aed047a3f7daa
İpek Efendiev <ipekefendiev@gmail.com>
2022-08-25 11:57:18 +0300

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 const Document = () => {
   return (
@@ -9,6 +10,20 @@ const Document = () => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❤️‍🔥</text></svg>"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W2VLQZGTVL"
+          strategy="lazyOnload"
+        ></Script>
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-W2VLQZGTVL')
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
